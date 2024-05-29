199b2fa3361012ee1936b114e69055fba13380e7
siknyang <167048578+siknyang@users.noreply.github.com>
2024-05-29 20:48:38 +0900

    Feature: 동적 환경 조사 기능 구현

---
+++ Assets/Scripts/Player/Interaction.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    public float checkRate = 0.05f;
+    private float lastChectkTime;
+    public float maxChectkDistance;
+    public LayerMask layerMask;
+
+    public GameObject curInteractGameObject;
+    private IInteractable curInteractable;
+
+    public TextMeshProUGUI promptText;
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Time.time - lastChectkTime > checkRate)
+        {
+            lastChectkTime = Time.time;
+
+            Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxChectkDistance, layerMask))
+            {
+                if (hit.collider.gameObject != curInteractGameObject)
+                {
+                    curInteractGameObject = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IInteractable>();
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractGameObject = null;
+                curInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = curInteractable.GetInteractPrompt();
+    }
+}
